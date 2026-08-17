29b6d2d567c50800c3ddf5ef90f6f82ceed6e9a1
Kolass2004 <raniv2057@gmail.com>
2026-08-17 15:31:21 +0530

    chore: bump version to 1.1.3 and refactor keystore property loading in build configuration

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,3 +1,6 @@
+import java.util.Properties
+import java.io.FileInputStream
+
 plugins {
     id("com.android.application")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
@@ -26,11 +29,11 @@ android {
     }
 
     val keystorePropertiesFile = rootProject.file("key.properties")
-    val keystoreProperties = java.util.Properties()
+    val keystoreProperties = Properties()
     var hasCustomKeystore = false
 
     if (keystorePropertiesFile.exists()) {
-        keystoreProperties.load(java.io.FileInputStream(keystorePropertiesFile))
+        keystoreProperties.load(FileInputStream(keystorePropertiesFile))
         hasCustomKeystore = true
     } else if (System.getenv("KEYSTORE_PATH") != null) {
         hasCustomKeystore = true
